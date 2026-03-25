21d131d3bfca1eb96af1a577cd3874a9c3c93013
Leonardo010207 <leonardopiloto4@gmail.com>
2026-03-25 15:58:21 -0300

    adicionando exercício de composição Edificio e Apartamentos

---
+++ EdificioApartamentos/src/TestaEdificioApartamento.java
@@ -0,0 +1,11 @@
+public class TestaEdificioApartamento {
+    public void main(String[] args) {
+        Edificio ed1 = new Edificio("Residencial Sol", "Rua A, 123");
+
+        ed1.construirApartamento(101, 1);
+        ed1.construirApartamento(202, 2);
+        ed1.construirApartamento(303, 3);
+
+        System.out.println(ed1.toString());
+    }
+}
